3b94de52559acf9338735c780fcc0f8cc48afdbf
Titus Wormer <tituswormer@gmail.com>
2021-08-05 15:52:16 +0200

    Remove warning for remark 12

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 import {directive} from 'micromark-extension-directive'
 import {directiveFromMarkdown, directiveToMarkdown} from 'mdast-util-directive'
 
-let warningIssued
-
 export default function remarkDirective() {
   const data = this.data()
-
-  // Old remark.
-  /* c8 ignore next 14 */
-  if (
-    !warningIssued &&
-    ((this.Parser &&
-      this.Parser.prototype &&
-      this.Parser.prototype.blockTokenizers) ||
-      (this.Compiler &&
-        this.Compiler.prototype &&
-        this.Compiler.prototype.visitors))
-  ) {
-    warningIssued = true
-    console.warn(
-      '[remark-directive] Warning: please upgrade to remark 13 to use this plugin'
-    )
-  }
 
   add('micromarkExtensions', directive())
   add('fromMarkdownExtensions', directiveFromMarkdown)
